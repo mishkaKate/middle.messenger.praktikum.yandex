5791dead487beb065409145a58c95c371757d4c2
MishKate <ekzemsk@gmail.com>
2026-08-06 12:52:31 +0300

    sprint_1: pages
    sprint_1: netlify
    
    sprint_1: delete svg
    
    Update netlify.toml
    
    Update README.md
    
    Update chats.ts with empty line
    
    sorint_1: empty lines

--- README.md
+++ README.md
@@ -15,5 +15,7 @@ git clone https://github.com/mishkaKate/middle.messenger.praktikum.yandex.git
 cd repo
 npm install
 npm run start
-
+```
 Проект будет доступен по адресу http://localhost:3000/
+
+ссылка на развёрнутый в Netlify проект https://zefir-chat.netlify.app/
---
+++ src/mocks/chats.ts
@@ -0,0 +1,97 @@
+export const chats = [
+  {
+    "id": 123,
+    "title": "чат разработки",
+    "avatar": "/123/avatar1.jpg",
+    "unread_count": 15,
+    "created_by": 12345,
+    "last_message": {
+      "user": {
+        "first_name": "Petya",
+        "second_name": "Pupkin",
+        "avatar": "/path/to/avatar.jpg",
+        "email": "my@email.com",
+        "login": "userLogin",
+        "phone": "8(911)-222-33-22"
+      },
+      "time": "2020-01-02T14:22:22.000Z",
+      "content": "привет! я Петя Пупкин!"
+    }
+  },
+  {
+    "id": 1234,
+    "title": "чат сладкоежек",
+    "avatar": "/1234/avatar1.jpg",
+    "unread_count": 153,
+    "created_by": 12345,
+    "last_message": {
+      "user": {
+        "first_name": "Katusha",
+        "second_name": "Matusha",
+        "avatar": "/path/to/avatar1.jpg",
+        "email": "myKatya@email.com",
+        "login": "katushMatusha",
+        "phone": "8(911)-222-35-22"
+      },
+      "time": "2020-01-02T14:22:22.000Z",
+      "content": "привет! пойдемте пить чай!"
+    }
+  },
+  {
+    "id": 1234,
+    "title": "чат сладкоежек чат сладкоежек long long chat name...чат сладкоежек long long chat nameчат сладкоежек long long chat name...чат сладкоежек long long chat name",
+    "avatar": "/1234/avatar1.jpg",
+    "unread_count": 153,
+    "created_by": 12345,
+    "last_message": {
+      "user": {
+        "first_name": "Katusha",
+        "second_name": "Matusha",
+        "avatar": "/path/to/avatar1.jpg",
+        "email": "myKatya@email.com",
+        "login": "katushMatusha",
+        "phone": "8(911)-222-35-22"
+      },
+      "time": "2020-01-02T14:22:22.000Z",
+      "content": "привет! пойдемте пить чай! чат сладкоежек long long chat name...чат сладкоежек long long chat nameчат сладкоежек long long chat name...чат сладкоежек long long chat nameчат сладкоежек long long chat name...чат сладкоежек long long chat nameчат сладкоежек long long chat name...чат сладкоежек long long chat name"
+    }
+  },
+  {
+    "id": 1234,
+    "title": "чат сладкоежек long long chat name...чат сладкоежек long long chat name",
+    "avatar": "/1234/avatar1.jpg",
+    "unread_count": 153,
+    "created_by": 12345,
+    "last_message": {
+      "user": {
+        "first_name": "Katusha",
+        "second_name": "Matusha",
+        "avatar": "/path/to/avatar1.jpg",
+        "email": "myKatya@email.com",
+        "login": "katushMatusha",
+        "phone": "8(911)-222-35-22"
+      },
+      "time": "2020-01-02T14:22:22.000Z",
+      "content": "привет! пойдемте пить чай!"
+    }
+  },
+  {
+    "id": 1234,
+    "title": "чат сладкоежек",
+    "avatar": "/1234/avatar1.jpg",
+    "unread_count": 153,
+    "created_by": 12345,
+    "last_message": {
+      "user": {
+        "first_name": "Katusha",
+        "second_name": "Matusha",
+        "avatar": "/path/to/avatar1.jpg",
+        "email": "myKatya@email.com",
+        "login": "katushMatusha",
+        "phone": "8(911)-222-35-22"
+      },
+      "time": "2020-01-02T14:22:22.000Z",
+      "content": "привет! пойдемте пить чай!  бтут будет длинное сообщение... тут будет длинное сообщение ... тут будет длинное сообщение"
+    }
+  }
+];
